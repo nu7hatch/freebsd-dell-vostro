18a75cc7b45e6f3a7b77ea98a445eaae92d59027
Kriss Kowalik <kriss.kowalik@gmail.com>
2010-09-06 19:31:31 +0200

    small fix in kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir ~/kernels
-fetch http://github.com/nu7hatch/freebsd-dell-vostro/raw/root/kernels/DELLVOSTRO -o ~/kernels/DELLVOSTRO
+fetch http://github.com/nu7hatch/freebsd-dell-vostro/raw/master/root/kernels/DELLVOSTRO -o ~/kernels/DELLVOSTRO
 cd /usr/src/sys/amd64/conf
 ln -s ~/kernels/DELLVOSTRO
 config DELLVOSTRO 
